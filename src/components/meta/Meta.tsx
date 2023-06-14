6d89f67e94fd974ee044da549c2d0d302c681a88
diyorbek-sultanov <diyorbeksultonov1@mail.ru>
2023-06-14 10:40:55 +0500

    favicon

--- src/components/meta/Meta.tsx
+++ src/components/meta/Meta.tsx
@@ -17,7 +17,29 @@ const Meta: FC<PropsWithChildren<IMeta>> = ({
 				<meta name='description' content={description} />
 				<meta name='og:title' content={getTitle(title)} />
 				<meta name='og:description' content={description} />
+				<meta name='og:url' content={process.env.NEXT_SITE_URL} />
 				<link rel='canonical' href={process.env.NEXT_SITE_URL} />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/favicon/apple-touch-icon.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='/favicon/favicon-32x32.png'
+				/>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='/favicon/favicon-16x16.png'
+				/>
+				<link rel='manifest' href='/favicon/site.webmanifest' />
+				<link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+				<meta name='msapplication-TileColor' content='#da532c' />
+				<meta name='theme-color' content='#ffffff' />
 			</Head>
 			{children}
 		</>
